6713e6f58e32440a1fb54172bf6fa25d9a0c03a6
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2020-06-15 18:17:42 -0500

    Card Properties Added Reactstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Retail from './components/RetailComponent';
+import { PRODUCTS } from './shared/products.js';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: PRODUCTS
+        }
+    }
   render() {
       return (
           <div className="App">
@@ -12,7 +19,7 @@ class App extends Component {
                   <NavbarBrand href="/">Tre Caffè</NavbarBrand>
               </div>
               </Navbar>
-              <Retail />
+              <Retail products={this.state.products} />
           </div>
       );
   }
--- src/components/RetailComponent.js
+++ src/components/RetailComponent.js
@@ -1,94 +1,27 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardSubtitle, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 
 class Retail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            products: [
-                {
-                    id: 0,
-                    name: 'Morning Blend',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Ground',
-                    size: '1 lb',
-                    description: "Start your morning off right with this medium roast that will carry you through your day."
-                },
-                {
-                    id: 1,
-                    name: 'Morning Blend',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Whole Bean',
-                    size: '1 lb',
-                    description: "Start your morning off right with this medium roast that will carry you through your day."
-                },
-                {
-                    id: 2,
-                    name: 'Brunch Blend',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Ground',
-                    size: '1 lb',
-                    description: "Need an afternoon pick me up? This will get you through the afternoon slump."
-                },
-                {
-                    id: 3,
-                    name: 'Brunch Blend',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Whole Bean',
-                    size: '1 lb',
-                    description: "Need an afternoon pick me up? This will get you through the afternoon slump."
-                },
-                {
-                    id: 4,
-                    name: 'Dark Roast',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Ground',
-                    size: '1 lb',
-                    description: "Let this dark roast boost your mood and spirits with an aroma and taste that won't disappoint."
-                },
-                {
-                    id: 5,
-                    name: 'Dark Roast',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Ground',
-                    size: '1 lb',
-                    description: "Let this dark roast boost your mood and spirits with an aroma and taste that won't disappoint."
-                },
-                {
-                    id: 6,
-                    name: 'Tre Caffè Apparel',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Hoodie',
-                    size: 'Various',
-                    description: "Show us your love for Tre Caffè!"
-                },
-                {
-                    id: 7,
-                    name: 'Tre Caffè',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Coffee Mug',
-                    size: 'large',
-                    description: "Let everyone know why you love your coffee so much."
-                },
-                {
-                    id: 8,
-                    name: 'I love Coffee',
-                    image: 'assets/images/comingsoon.jpeg',
-                    style: 'Coffee Mug',
-                    size: 'large',
-                    description: "Because everyone knows coffee is life."
-                },                                             
-            ]
+
         };
     }
     
         render() {
-            const retail = this.state.products.map(product => {
+            const retail = this.props.products.map(product => {
                 return (
-                    <div key={product.id} className="col">
-                        <img src={product.image} alt={product.name} />
-                        <h2>{product.name}</h2>
-                        <h4>{product.style}</h4>
-                        <p>{product.description}</p>
+                    <div key={product.id} className="col-md-5 m-1">
+                        <Card>
+                            <CardImg width="100%" src={product.image} alt={product.name} />
+                            <CardBody>
+                                <CardTitle>{product.name}</CardTitle>
+                                <CardSubtitle>{product.style}</CardSubtitle>
+                                <CardText>{product.description}</CardText>
+                                <Button>Buy</Button>
+                            </CardBody>
+                        </Card>
                     </div>
                 );
             });
