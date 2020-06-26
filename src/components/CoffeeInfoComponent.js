84ef00138ffd56691d9b49c5a470b351f7dfd50a
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2020-06-26 15:56:52 -0500

    make these changes

---
+++ src/components/CoffeeInfoComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+
+class Coffee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           funinfo: [
+            {
+                id: 0,
+                image: "./assets/images/caffe_sign.jpg",
+                name: "neon sign",
+                title: "Coffee Quotes",
+                message: "Everyone should beleive in something. I believe I will have another coffee.",
+                btntext: "Click for more quotes!",
+                link: "https://www.thebaristalife.com/blogs/blog/barista-lifes-top-117-coffee-quotes"
+            },
+            {
+                id: 1,
+                image: "/assets/images/coffee_rawbean.jpg",
+                name: "raw coffee bean",
+                title: "Coffee Facts",
+                message: "Did you know that coffee beans are technically seeds! They are the ptis of cherry-like berries. Plus, the berries can be eaten as food!",
+                btntext: "Click for fun facts!",
+                link: "https://www.goodhousekeeping.com/health/diet-nutrition/a30303/facts-about-coffee/",
+            }
+           ],
+        };
+    }
+    
+        render() {
+            const coffee = this.state.funinfo.map(info => {
+                return (
+                    <div key={info.id} className="col-md-5 m-1" >
+                        <Card className="center" id="cardQF">
+                            <CardImg width="100%" src={info.image} alt={info.name} />
+                            <CardBody>
+                                <CardTitle id="cardTitleQF">{info.title}</CardTitle>
+                                <CardText>{info.message}</CardText>
+                                <Button className="btn btn-dark" id="cardHomeBtn" href={info.link}>{info.btntext}</Button>
+                            </CardBody>
+                        </Card>
+                    </div> 
+                );
+            });
+
+            return (
+                <div className="container">
+                    <div className="row">
+                        {coffee}
+                    </div>
+                </div>
+            );
+        }
+}
+
+export default Coffee;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HomeCarousel } from './CarouselComponent';
-import FunCoffee from './CoffeeInfo';
+import Coffee from './CoffeeInfoComponent';
 
 
 function Home(props) {
@@ -10,15 +10,26 @@ function Home(props) {
              <div className="container-fluid p-0 m-0">
                 <img class="img-fluid" src="./assets/images/home_bg_nav.jpg" alt="coffee"/>
             </div>
+<<<<<<< HEAD
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-3">
                         <HomeCarousel />
                     </div>
                 </div>
-                <div className="containter">
+                <div className="container">
                     <FunCoffee funinfo={this.state.funinfo}/>
                 </div>
+=======
+            <div className="container-fluid p-0 m-0">
+                <HomeCarousel />
+            </div>
+            <div className="container" id="cardQF">
+                <Coffee />
+            </div>
+            <div className="container-fluid p-0 m-0">
+                <img class="img-fluid" src="./assets/images/img2.jpg" alt="coffee"/>
+>>>>>>> 24da5bf11da0c55f2ada07a4dc57d83df683e306
             </div>
         </React.Fragment>
        
