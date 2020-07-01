9a3805baad82bb0dfc10e9f8409877294cd871c8
Natasha Douglas <ncdouglas2016@gmail.com>
2020-06-30 20:13:06 -0500

    Location Component
    added map

--- src/components/LocationComponent.js
+++ src/components/LocationComponent.js
@@ -1,47 +1,25 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
-import React, { Component, createRef } from 'react';
+class Location extends Component {
+  render(){
 
-class GoogleMap extends Component {
-  googleMapRef = React.createRef()
+    const GoogleMapLocation = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter = {{ lat: 47.610144, lng: -122.342551 }}
+        defaultZoom = { 13 } 
+      >
+      </GoogleMap>
+    ));
 
-  componentDidMount() {
-    const googleMapScript = document.createElement('script')
-    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`
-    window.document.body.appendChild(googleScript)
-
-    googleScript.addEventListener('load', {
-     this.googleMap = this.createGoogleMap(),
-      marker = this.createMarker()
-    })
+    return(
+      <div>
+        <GoogleMapLocation
+          containerElement={ <div style={{ height: '500px', width: '500px' }}/>}
+          mapElement={ <div style={{ height: '100%' }}/>}
+        />
+      </div>
+    );
   }
-
-  createGoogleMap = () =>
-    new window.google.maps.Map(this.googleMapRef.current, {
-      zoom: 16,
-      center: {
-        lat: 43.642567,
-        lng: -79.387054,
-      },
-      disableDefaultUI: true,
-    })
-
-  createMarker = () =>
-    new window.google.maps.Marker({
-      position: { lat: 43.642567, lng: -79.387054 },
-      map: this.googleMap,
-    })
-
-  render() {
-    return (
-      <div
-        id="google-map"
-        ref={this.googleMapRef}
-        style={{ width: '400px', height: '300px' }}
-      />
-    )
-  }
-}
-
-
-
-export default GoogleMap;
+};
+export default Location;
