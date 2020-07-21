7cbb75d487b2b019014daa18068573696c3cef8e
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2020-07-20 19:21:43 -0500

    Location Updated
    Removed Card from imports.... wasn't used in code

--- src/components/LocationComponent.js
+++ src/components/LocationComponent.js
@@ -1,36 +1,35 @@
-// import React, { Component } from 'react';
-// import { Card, CardBody, CardImg, CardTitle, CardText } from 'reactstrap';
-// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-// class Location extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class Location extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-//   render(){
+  render(){
 
-//     return(
-//         <div style={{position: "relative", height: "500px"}}>
-//           <Map 
-//             google={this.props.google}
-//             zoom={15}
-//             style={mapStyles}
-//             initialCenter={{ lat: 47.610144, lng: -122.342551 }}
-//           >
-//             <Marker position={{ lat: 47.610144, lng: -122.342551 }} />
-//           </Map>
+    return(
+        <div style={{position: "relative", height: "500px"}}>
+          <Map 
+            google={this.props.google}
+            zoom={15}
+            style={mapStyles}
+            initialCenter={{ lat: 47.610144, lng: -122.342551 }}
+          >
+            <Marker position={{ lat: 47.610144, lng: -122.342551 }} />
+          </Map>
           
-//         </div>
-//     );
-//   }
-// };
+        </div>
+    );
+  }
+};
 
-// const mapStyles = {
-//   position: 'relative', 
-//   width: '100%',
-//   height: '500px',
-// };
+const mapStyles = {
+  position: 'relative', 
+  width: '100%',
+  height: '500px',
+};
 
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyCzAQENrT-cAfzhOfDkWPhL1X8NowRXDf8'
-// })(Location);
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCzAQENrT-cAfzhOfDkWPhL1X8NowRXDf8'
+})(Location);
