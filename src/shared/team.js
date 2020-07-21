3d982a8945b1d076ecf8082db5457ba925c6b6ad
Natasha Douglas <ncdouglas2016@gmail.com>
2020-07-21 09:14:01 -0500

    cleanup

--- src/shared/team.js
+++ src/shared/team.js
@@ -3,13 +3,13 @@ export const TEAMMEMBERS = [
     {
         id: 0,
         name: 'Natasha Douglas',
-        image: 'assets/images/natasha.png',
+        image: 'assets/images/team/natasha.png',
         description: 'Natasha is a Jr Web and Mobile Developer currently working on her Bootcamp Fullstack Graduation Certificate. '
     },
     {
         id: 1,
         name: 'Becky Richardson',
-        image: 'assets/images/becky.png',
+        image: 'assets/images/team/becky.png',
         description: 'Becky is a Jr Web and Mobile Developer currently working on her Bootcamp Fullstack Graduation Certificate.'
     }
 ];
